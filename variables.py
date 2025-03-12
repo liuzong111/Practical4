28bdf4101f63dcd6dc8dc4c7fff99fe6d7953f68
liuzong111 <3299627668@qq.com>
2025-03-12 14:42:07 +0800

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,15 @@
+a=15
+b=75
+c=a+b
+d=90
+e=5
+f=d+e
+h="The faster way is to "
+if c==f:
+    print ("The two ways are equal.")
+elif c>f:
+    g="walk to the bus stop and take the bus directly to their office."
+    print(h+g)
+else:
+    g="drive to a nearby car park and then walk the final stage."
+    print(h+g)
