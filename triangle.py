ac5606c155d3874f88fdc563875791b0e3289bc3
liuzong111 <3299627668@qq.com>
2025-03-14 11:31:59 +0800

    Add files via upload

--- triangle.py
+++ triangle.py
@@ -1,6 +1,6 @@
-#calculate the 1+2+...+n
-n=int(input())
-T=0
-for i in range(n+1):
-    T+=i
-print("T"+str(n)+"="+str(int(T)))
+#calculate the 1+2+...+n (n=1,2,...,10)
+for n in range(1,11):
+    T=0
+    for i in range(n+1):
+        T+=i
+    print("T"+str(n)+"="+str(int(T)))
